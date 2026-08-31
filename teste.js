bdb5dcada3123c69c7bddb08748b11421e9f6508
Benellinho <1Lucasbenelli@gmail.com>
2026-08-31 19:36:12 -0300

    Implementação do método da falsa posição

--- teste.js
+++ teste.js
@@ -1,4 +1,5 @@
-import metodo from "./Metodos/Bisseção.js";
+import Bisseção from "./Metodos/Bisseção.js";
+import { Falsa_Posição } from "./Metodos/False posição.js";
 
 // Altere os valores abaixo para realizar um novo teste.
 const função = (x) => 4 * Math.cos(x)   -(Math.E ** 2 * x);
@@ -6,10 +7,14 @@ const a = -3;
 const b = 1;
 const erro = 0.001;
 
-const resultado = metodo.Bisseção(função, a, b, erro);
+const resultado1 = Bisseção.Bisseção(função, a, b, erro);
+const resultado2 = Falsa_Posição(função, a, b, erro);
 
 console.log("Parâmetros do teste:");
 console.table({ a, b, erro });
 
 console.log("Resultado do método da bisseção:");
-console.table(resultado);
+console.table(resultado1);
+
+console.log("Resultado do método da falsa posição:");
+console.table(resultado2);
